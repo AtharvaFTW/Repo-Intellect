eb84c6b9e9a5f7f566aa1dc2a5169c8ba2443e82
AtharvaFTW <atharvavankhede@gmail.com>
2025-09-13 13:38:20 +0530

    added Module 6

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from repository_processor import clone_github_repo, load_and_chunk
+from vector_store_manager import VectorStoreManager
+from main_crew import RepoAnalysisCrew
+
+st.set_page_config(page_title="Repo-Intellect",layout="wide")
+st.title("🧠 Repo-Intellect: AI GitHub Repository Analysis Agent")
+
+st.sidebar.header("Instructions")
+st.sidebar.info("""
+            1.Enter a public GitHub repository URL.\n
+            2.Provide a high-level query or question about the repository.\n
+            3.Click 'Analyze Repository' to start the analysis. \n
+            \n
+            This process may take several minutes depending on the repositoy size.
+
+                """)
+repo_url=st.text_input("Enter a GitHub Repository URL:", placeholder="https://github.com/user/repo")
+user_query=st.text_area("What do you want to know abouth the shared repository?", placeholder="e.g., Provide an analysis of the repository.")
+
+if st.button("Analyze Repository"):
+    if not repo_url:
+        st.error("Please provide a repository URL")
+    elif not user_query:
+        st.error("Please provide a query")
+    else:
+        try:
+            with st.spinner("Step 1/4: Cloning the shared repository."):
+                repo_path=clone_github_repo(repo_url=repo_url)
+                st.success("Repository clone successfully!")
+
+            with st.spinner("Step 2/4: Processing and chunking files..."):
+                documents=load_and_chunk(repo_path)
+                st.success("Files processed and chunked!")
+            
+            with st.spinner("Step 3/4: Creating vector store and retriever..."):
+                collection_name=f"repo_{repo_url.split('/')[-1]}"
+                vector_store_manager=VectorStoreManager(collection_name=collection_name)
+                vector_store_manager.populate_vector_store(documents)
+                retreiver=vector_store_manager.get_retriever()
+                st.success("Vector store created!")
+            
+            with st.spinner("Step 4/4: The AI crew is analyzing the repository. This may take a while."):
+                analysis_crew=RepoAnalysisCrew(retriever=retreiver)
+                result= analysis_crew.run(repo_path=repo_path, user_query=user_query)
+                st.success("Analysis Complete!")
+            
+            st.header("Anaysis Report")
+            st.markdown(result)
+
+        except Exception as e:
+            st.error(f"An error occured: {e}")
+            
